5d014884ea3eb8e36852d9abfecb47473e2868de
Joyce <joycesantos8900@gmail.com>
2025-02-01 10:38:50 -0300

    alterações

---
+++ pb_migrations/1738191061_updated_produtos.js
@@ -0,0 +1,26 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1135311916")
+
+  // remove field
+  collection.fields.removeById("editor1579384326")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1135311916")
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "convertURLs": false,
+    "hidden": false,
+    "id": "editor1579384326",
+    "maxSize": 0,
+    "name": "name",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "editor"
+  }))
+
+  return app.save(collection)
+})
